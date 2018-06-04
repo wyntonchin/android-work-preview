cef1ca67ece9c511804ec405bfc533a3f5024f4d
wyntonchin <goodcar@163.com>
2018-06-04 19:49:20 +0800

    完成智能页面的效果图

--- app/src/main/java/com/hismart/easylink/preview/ui/launch/IntelligenceFragment.java
+++ app/src/main/java/com/hismart/easylink/preview/ui/launch/IntelligenceFragment.java
@@ -2,7 +2,11 @@ package com.hismart.easylink.preview.ui.launch;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +14,24 @@ import android.view.ViewGroup;
 
 import com.hismart.easylink.preview.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class IntelligenceFragment extends Fragment {
+    /**
+     * mViewContent 缓存视图内容
+     */
+    private View mViewContent;
 
-    private View mViewContent; // 缓存视图内容
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+    private ArrayList<String> mTitles = new ArrayList<>();
+    private FragmentManager fragmentManager;
+
     public IntelligenceFragment() {
         // Required empty public constructor
     }
@@ -26,9 +42,10 @@ public class IntelligenceFragment extends Fragment {
                              Bundle savedInstanceState) {
         if (mViewContent == null) {
             mViewContent = inflater.inflate(R.layout.fragment_main_intelligence, container, false);
-            //mToolbar = mViewContent.findViewById(R.id.toolbar);
-            // 代替Actionbar
-            //((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
+            tabLayout = mViewContent.findViewById(R.id.tab_layout);
+            viewPager = mViewContent.findViewById(R.id.view_pager);
+            //viewPager.setOffscreenPageLimit(2);//指定加载的页数，不回收数据
+            initTabLayout();
         }
         // 缓存View判断是否含有parent, 如果有需要从parent删除, 否则发生已有parent的错误.
         ViewGroup parent = (ViewGroup) mViewContent.getParent();
@@ -38,4 +55,75 @@ public class IntelligenceFragment extends Fragment {
         return mViewContent;
     }
 
+    private void initTabLayout() {
+
+        mTitles.add("常用");
+        mTitles.add("我的");
+
+        fragmentManager = getChildFragmentManager();
+        //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+        fragmentList.add(new CommonSenceFragment());
+        fragmentList.add(new CommonSenceFragment());
+
+
+
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(fragmentManager, fragmentList, mTitles);
+        tabLayout.setSelected(true);
+        //tabLayout.setTabsFromPagerAdapter(myPagerAdapter);
+        viewPager.setAdapter(myPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //viewPager.setCurrentItem(tab.getPosition());
+                viewPager.setCurrentItem(tab.getPosition(),false);//false取消自动切换时的滚动动画
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        ArrayList<Fragment> fragmentsList;
+        List<String> titles;
+
+        public MyPagerAdapter(FragmentManager fm, ArrayList<Fragment> fragmentsList, ArrayList<String> titles) {
+            super(fm);
+            this.fragmentsList = fragmentsList;
+            this.titles = titles;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentsList.get(position);
+        }
+
+
+        @Override
+        public int getCount() {
+            return fragmentsList.size();
+        }
+
+        //这里设置的是tablayout的title标题内容
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles.get(position);
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return super.getItemPosition(object);
+        }
+    }
 }
